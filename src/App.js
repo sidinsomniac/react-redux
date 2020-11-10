6e418a2457b60cb3b0128a1c20ea9d833043a43f
Siddhartha Chatterjee <35187839+sidinsomniac@users.noreply.github.com>
2020-11-10 22:37:28 +0530

    Use connect

--- src/App.js
+++ src/App.js
@@ -1,11 +1,15 @@
 import React from "react";
 import "./styles.css";
 import Cake from "./components/Cake";
+import { Provider } from "react-redux";
+import store from "./redux/cake/store";
 
 export default function App() {
   return (
-    <div className="App">
-      <Cake />
-    </div>
+    <Provider store={store}>
+      <div className="App">
+        <Cake />
+      </div>
+    </Provider>
   );
 }
--- src/components/Cake.js
+++ src/components/Cake.js
@@ -1,10 +1,22 @@
 import React from "react";
+import { connect } from "react-redux";
+import { buyCake } from "../redux";
 
-export default function Cake() {
+function Cake(props) {
   return (
     <div>
-      <h2>Number of cakes</h2>
-      <button>Buy cakes</button>
+      <h2>Number of cakes: {props.numberOfCakes}</h2>
+      <button onClick={props.buyCake}>Buy cakes</button>
     </div>
   );
 }
+
+const mapStateToProps = (state) => ({
+  numberOfCakes: state.numberOfCakes
+});
+
+const mapDispatchToProps = (dispatch) => ({
+  buyCake: () => dispatch(buyCake())
+});
+
+export default connect(mapStateToProps, mapDispatchToProps)(Cake);
